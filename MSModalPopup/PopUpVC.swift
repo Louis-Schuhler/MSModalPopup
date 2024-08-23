562531b150abacff45ba32c632748f7b3f513844
Louis Schuhler 🤿 <Louis.Schuhlerpro@gmail.com>
2024-08-23 16:42:27 +0200

    Create PopUpVC.swift

---
+++ MSModalPopup/PopUpVC.swift
@@ -0,0 +1,13 @@
+import UIKit
+
+class PopUpVC: ModalVC {
+    
+    //In this file you can populate your costum modal popup
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+}
